15e37b877cfbba73c51064ec4c47c56c0ed072e2
daisybaicai <360619895@qq.com>
2020-09-12 16:51:35 +0800

    feat: add dependencies, lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,13 +5,28 @@ module.exports = {
   },
   extends: [
     'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
     '@vue/standard'
   ],
   parserOptions: {
     parser: 'babel-eslint'
   },
+  plugins: ['vue'],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 0,
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true,
+        trailingComma: 'none',
+        bracketSpacing: true,
+        jsxBracketSameLine: true,
+        semi: false
+      }
+    ],
+    'no-undef': [0]
   }
 }
